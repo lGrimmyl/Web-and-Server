29e1f492e3225749067bb6e5bd8b2617ddc881af
DgUcab <dgucab@gmail.com>
2023-06-23 22:28:05 +0800

    Remodel and working authentication and log in

--- src/Pages/Profle.js
+++ src/Pages/Profle.js
@@ -1,9 +1,20 @@
 import { UserInfo } from '../Components/api';
 import { useQuery } from 'react-query'; 
 import logcon from '../Images/log1.png';
-
+import bg from '../Images/bg1.png';
+import map from '../Images/map.png';
+import chat from '../Images/chat.png';
+import org from '../Images/Org.png';
+import eve from '../Images/Events.png'
+import { Card,ButtonBase,CardContent,FormControl, Divider } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile() {
+  const navigate = useNavigate();
+  const handleCardClick = () => {
+    navigate('/other-link'); // Replace '/other-link' with the desired route path
+  };
+
   const { data, isLoading, error } = useQuery("user", UserInfo, {
     retry: 0,
     onSuccess: (data) => console.log(data),
@@ -22,14 +33,105 @@ export default function Profile() {
     );
   }
   return (
-    <div style={{ backgroundColor: '#425c59', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <img src={logcon} style={{ width: 350, height: 350, marginRight: 10 }} />
-      <div style={{ display: 'flex', flexDirection: 'column', margin: 0, padding: 0 }}>
-      <h1 style={{marginTop: -30, marginLeft: -10 ,color: '#ffcc9f', fontSize: 80 }}>User Profile</h1>
-        <h1 style={{ color: '#ffcc9f',marginTop: -50, marginBottom: 0,  }}>{data.first_name} {data.last_name}</h1>
-        <p style={{ color: '#ffcc9f', marginTop: 0, marginBottom: 0 }}>{data.email}</p>
-        <p style={{ color: '#ffcc9f',marginTop: 0, marginBottom: 0 }}>User ID: {data.id}</p>
+    <div style={{
+      minHeight: '100vh',
+      backgroundImage: `url(${bg})`,
+      backgroundSize: 'cover',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}>
+      <img src={logcon} style={{ width: 150, height: 150,marginLeft:1150,marginBottom:-20}} />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <h1 style={{marginRight:1300,marginBottom:500,marginTop:-100,color: '#FFF', fontSize: 80 }}>Hello, {data.first_name}</h1>
+        <h1 style={{ color: '#FFF', marginLeft:1550,marginTop:-600  }}>{data.first_name} {data.last_name}</h1>
+        <p style={{ color: '#FFF', marginLeft:1570, marginTop: -10}}>{data.email}</p>
+        <p style={{ color: '#FFF',marginLeft:1590 , marginTop: -55 }}>User ID: {data.id}</p>
       </div>
+      
+      <ButtonBase onClick={() => {navigate('/CampusMap')}}>
+          <Card style={{ borderRadius:30,width: 281, height: 247, margin: '10px', backgroundColor: '#2B59A2',marginLeft:-1100, marginBottom:-400 }}>
+            <CardContent>
+              <FormControl>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <span style={{  color: 'white', fontSize: 20, marginTop: 80}}>Campus Map</span>
+                  <img src={map} style={{ width: 400, height: 400, marginTop: -70,marginLeft:-50 }} alt="Map Image" />
+                </div>
+              </FormControl>
+            </CardContent>
+          </Card>
+        </ButtonBase>
+
+  <ButtonBase onClick={() => {navigate('/Chat')}}>
+  <Card style={{borderRadius:30,width: 281, height: 247, margin: '10px', backgroundColor: '#FAD567',marginLeft:-500,marginBottom: -400 }}>
+    <CardContent>
+      <FormControl>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+            <span style={{ color: 'white', fontSize: 20, marginTop: 100, display: 'inline-block', maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>Chat</span>
+          </div>
+          <div style={{ marginRight: '10px' }}>
+            <img src={chat} style={{  width: 250, height: 225,marginBottom:70,marginLeft:-20 }} alt="Map Image" />
+          </div>
+        </div>
+      </FormControl>
+    </CardContent>
+  </Card>
+</ButtonBase>
+
+<ButtonBase onClick={() => {navigate('/Organizations')}}>
+  <Card style={{borderRadius:30,width: 281, height: 247, margin: '10px', backgroundColor: '#42AEB0' ,marginBottom:-940, marginLeft:-1100}}>
+    <CardContent>
+      <FormControl>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+            <span style={{ color: 'white', fontSize: 20, marginTop: 100, display: 'inline-block', maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>Organizations</span>
+          </div>
+          <div style={{ marginRight: '10px' }}>
+            <img src={org} style={{  width: 200, height: 200,marginBottom:120,marginLeft:-75 }} alt="Map Image" />
+          </div>
+        </div>
+      </FormControl>
+    </CardContent>
+  </Card>
+</ButtonBase>
+
+<ButtonBase onClick={() => {navigate('/EventUp')}}>
+  <Card style={{borderRadius:30,width: 281, height: 247, margin: '10px', backgroundColor: '#FA7470' ,marginBottom:-940,marginLeft:-500}}>
+    <CardContent>
+      <FormControl>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+            <span style={{ color: 'white', fontSize: 20, marginTop: 100, display: 'inline-block', maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>Events</span>
+          </div>
+          <div style={{ marginRight: '10px' }}>
+            <img src={eve} style={{  width: 300, height: 250,marginLeft:-80,marginTop:-30,marginBottom:100 }} alt="Map Image" />
+          </div>
+        </div>
+      </FormControl>
+    </CardContent>
+  </Card>
+</ButtonBase>
+
+<ButtonBase onClick={() => {navigate('/EventUp')}}>
+  <Card style={{borderRadius:30,width: 605, height: 512, margin: '10px', backgroundColor: '#FFF' ,marginBottom:-670,marginRight:-700}}>
+    <CardContent>
+      <FormControl>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+            <span style={{ color: '#3BC98D', fontSize: 30, marginTop: 5, display: 'inline-block',  overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>My Upcoming Events</span>
+            
+          </div>
+          
+        </div>
+      </FormControl>
+      <Divider />
+    </CardContent>
+  </Card>
+</ButtonBase>
+
+
+      
     </div>
   );
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -2,84 +2,110 @@ import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { CardContent,  FormControl,Card } from '@material-ui/core';
+import { CardContent, FormControl, Card } from '@material-ui/core';
 import axios from '../plugins/axios';
+import bg from '../Images/background.png';
+import logo from '../Images/blaze_1.png';
 
-function Register(){
-    const navigate = useNavigate()
-    const [login,setLogin] = useState({
-        'email' : '',
-        'password' : '',
-    })
-    const [feedback, setFeedback] = useState('')
-    const [errorPassword,setErrorPassword] = useState ('')
-    
-    return (
-        <div style={{ minHeight: '100vh', backgroundColor: '#425c59', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h1 style={{ marginTop: '150px', color: '#ffcc9f', fontSize: 50 }}>IV OF HEARTS</h1>
-            <Card style={{ width: '400px', height: '300px', backgroundColor: "#4b6966" }}>
-                <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <FormControl fullWidth>
-                        <TextField 
-                            style={{margin:5}}
-                            variant='outlined' 
-                            label='Email'
-                            value={login.email}
-                            InputProps={{style: { color: 'white' }  }}
-                            onChange={(event)=>{
-                                setLogin({
-                                    ...login,
-                                    email: event.target.value   
-                                })
-                            }}>
-                        </TextField>
-                        <TextField 
-                            style={{margin:5, color: 'white'}}
-                            variant='outlined' 
-                            label='Password'
-                            value={login.password}
-                            type='password'
-                            InputProps={{style: { color: 'white' }  }}
-                            onChange={(event)=>{
-                                setLogin({
-                                    ...login,
-                                    password: event.target.value   
-                                })
-                            }}>
-                        </TextField>
 
-                        <Button 
-                            style={{backgroundColor: '#ffcc9f', margin:5, marginTop: 25}}
-                            variant='contained'
-                            size='Large'
-                            onClick={()=> {
-                                axios.post('accounts/token/login',login).then(async (response) => {
-                                    console.log(response.data) 
-                                    localStorage.setItem('token', JSON.stringify(response.data.auth_token))
-                                    setFeedback('')
-                                    navigate('/Profile')
-                                }).catch((error)=>{
-                                    setFeedback('Invalid login. Please try again')
-                                })
-                                
-                            }}> 
-                            Login
-                        </Button>
-                        <Button 
-                            color='success'
-                            style={{backgroundColor: '#ffcc9f', margin:5}}
-                            variant='contained'
-                            size='Large'
-                            onClick={()=>{}}>
-                            Create Account
-                        </Button>
-                    </FormControl>
 
-                    <h3 style={{ color: 'white', fontSize: 13, fontStyle: 'italic', display: 'flex', alignItems: 'center'}} >{feedback} </h3>
-                </CardContent>
-            </Card>
+function Register() {
+  const navigate = useNavigate();
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+  const [feedback, setFeedback] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        backgroundImage: `url(${bg})`,
+        backgroundSize: 'cover',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+
+<div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginBottom: '-30px' }}>
+        <img src={logo} alt="Blaze Logo" style={{ width: '250px'}} />
         </div>
-    )
+        <h1 style={{ marginTop: '30px', color: '#ffffff', fontSize: 50 }}>Welcome to Blaze!</h1>
+     
+      <FormControl >
+        <TextField
+            style={{ margin: '10px', width: '400px' }} // Adjust the width here
+            variant="outlined"
+            label="Email"
+            value={login.email}
+            InputProps={{ style: { borderRadius: '20px', backgroundColor: '#FFFFFF' } }}
+            inputProps={{ size: 'small' }}
+          onChange={(event) => {
+            setLogin({
+              ...login,
+              email: event.target.value,
+            });
+          }}
+        />
+        <TextField
+         style={{ margin: '10px', width: '400px' }} // Adjust the width here
+         variant="outlined"
+         type='password'
+         label="Password"
+         value={login.password}
+         InputProps={{ style: { borderRadius: '20px', backgroundColor: '#FFFFFF' } }}
+         inputProps={{ size: 'small' }}
+          onChange={(event) => {
+            setLogin({
+              ...login,
+              password: event.target.value,
+            });
+          }}
+        />
+
+        <Button
+          style={{ borderRadius: '20px', backgroundColor: '#266AB3', margin: 5, marginTop: 25 }}
+          variant="contained"
+          size="large"
+          onClick={() => {
+            axios
+              .post('accounts/token/login', login)
+              .then(async (response) => {
+                console.log(response.data);
+                localStorage.setItem('token', JSON.stringify(response.data.auth_token));
+                setFeedback('');
+                navigate('/Profile');
+              })
+              .catch((error) => {
+                setFeedback('Invalid login. Please try again');
+              });
+          }}
+        >
+          Login
+        </Button>
+        <Button
+          color="success"
+          style={{ borderRadius: '20px', backgroundColor: '#266AB3', margin: 5, }}
+          variant="contained"
+          size="large"
+          onClick={() => {
+            navigate('/FRegister')
+          }}
+        >
+          Create an Account
+        </Button>
+        
+      </FormControl>
+
+      <h3 style={{ color: 'white', fontSize: 13, fontStyle: 'italic', display: 'flex', alignItems: 'center' }}>
+        {feedback}{' '}
+      </h3>
+    </div>
+  );
 }
 
 export default Register;
+
